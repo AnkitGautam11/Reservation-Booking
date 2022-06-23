392d2c56b78827c40e23d44852074da290bf9832
Ankit <gautamankit.0011@gmail.com>
2022-06-24 00:05:31 +0530

    last commit of frontend

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { 
   faBed,
@@ -14,6 +15,8 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { format } from 'date-fns';
 
 const Header = ({type}) => {
+
+  const [destination, setDestination] = useState("")
 
   const[openDate, setOpenDate] = useState(false);
 
@@ -33,6 +36,8 @@ const Header = ({type}) => {
 
   const[openOptions, setOpenOptions] = useState(false);
 
+  const navigate = useNavigate();
+
   const handleOption = (name, operation) => {
     setOptions(prev=>{
       return{
@@ -42,9 +47,13 @@ const Header = ({type}) => {
     });
   };
 
+  const handleSearch = () => {
+    navigate("hotels", {state: { destination, date, options }})
+  }
+
   return (
     <div className='header'>
-      <div className={type == "list" ? "headerContainer listMode" : "headerContainer"}>
+      <div className={type === "list" ? "headerContainer listMode" : "headerContainer"}>
         <div className="headerList">
           <div className="headerListItem active">
             <FontAwesomeIcon icon={ faBed }/>
@@ -84,6 +93,7 @@ const Header = ({type}) => {
                 type="text"
                 placeholder='Where are you going?'
                 className='headerSearchInput'
+                onChange={e => setDestination(e.target.value)}
               />
             </div>
             <div className="headerSearchItem">
@@ -156,7 +166,7 @@ const Header = ({type}) => {
               </div>}
             </div> 
             <div className="headerSearchItem">
-              <button className="headerBtn">Search</button>
+              <button className="headerBtn" onClick={handleSearch}>Search</button>
             </div>
           </div>
         </>)}
